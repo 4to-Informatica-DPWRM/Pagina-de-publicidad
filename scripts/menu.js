9ae6e62e8aa3da599be0e0a52b84df4f32af06f7
Greem3 <ian.pichardo@hotmail.com>
2024-04-10 09:29:55 -0400

    Menu en las paginas
    CSS del menu y JS en todas las paginas.

---
+++ scripts/menu.js
@@ -0,0 +1,26 @@
+//#*    [ VARIABLES]
+
+const documento = document.documentElement
+const menu = `<header>
+
+<img src="assets/img/logo.png" alt="Logo" class="logo">
+
+<div id="menu">
+    <p>Inicio</p>
+    <p>Supermercado</p>
+    <p>Farmacia</p>
+    <p>Loteria</p>
+    <p>Cine</p>
+    <p>Mercancia</p>
+    <p>Nosotros</p>
+</div>
+
+<div id="optinicio">
+    <p>Mision</p>
+    <p>Vision</p>
+    <p>Valores</p>
+</div>
+
+</header>`
+
+documento.innerHTML += menu
