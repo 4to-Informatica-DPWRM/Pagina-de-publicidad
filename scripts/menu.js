36d356e3b17b389df843879c376ed0c657b96d21
Greem3 <ian.pichardo@hotmail.com>
2024-04-17 09:10:32 -0400

    Menu actualizado
    Funciones y css

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,78 @@
-//#*    [ VARIABLES]
+//#*    [ VARIABLES ]
 
-const documento = document.documentElement
+const documento = document.documentElement;
+const documentostyle = window.getComputedStyle(document.documentElement);
 const menu = `<header>
 
-<img src="assets/img/logo.png" alt="Logo" class="logo">
+<img src="/assets/img/valores.webp" alt="Logo" class="logo">
 
 <div id="menu">
-    <p>Inicio</p>
-    <p>Supermercado</p>
-    <p>Farmacia</p>
-    <p>Loteria</p>
-    <p>Cine</p>
-    <p>Mercancia</p>
-    <p>Nosotros</p>
+    <p class="menuopts" id="1">Inicio</p>
+    <p class="menuopts" id="2">Supermercado</p>
+    <p class="menuopts" id="3">Farmacia</p>
+    <p class="menuopts" id="4">Loteria</p>
+    <p class="menuopts" id="5">Cine</p>
+    <p class="menuopts" id="6">Mercancia</p>
+    <p class="menuopts" id="7">Nosotros</p>
+
+    <div id="optinicio" class="opciones">
+        <p>Mision</p>
+        <p>Vision</p>
+        <p>Valores</p>
+    </div>
+
+    <div id="optsuper" class="opciones">
+        <p>Aprecio</p>
+        <p>Bravo</p>
+        <p>Sirena</p>
+        <p>Supermix</p>
+        <p>Super mercado nacional</p>
+    </div>
+
+    <div id="optfarm" class="opciones">
+        <p>Los Hidalgos</p>
+        <p>Farmacia Carolx</p>
+        <p>Farmacia GBC</p>
+        <p>Farmanax</p>
+    </div>
 </div>
 
-<div id="optinicio">
-    <p>Mision</p>
-    <p>Vision</p>
-    <p>Valores</p>
-</div>
+</header>`;
 
-</header>`
+documento.innerHTML += menu;
 
-documento.innerHTML += menu
+var objectid
+const menuhead = document.querySelectorAll('.menuopts');
+const iniciooptions = document.getElementById('optinicio');
+const opciones = document.querySelectorAll('.opciones');
+
+for (i = 0; i <= menuhead.length-1; i++) {
+    let object = menuhead.item(i);
+    let objectopt = opciones.item(i);
+
+    object.addEventListener('mouseover', function() {
+        objectid = Number(event.target.id)
+        mostrar_ocultar()
+    });
+
+    object.addEventListener('mouseout', function() {
+        mostrar_ocultar()
+    })
+
+    objectopt.addEventListener('mouseover', function() {
+        mostrar_ocultar()
+    })
+
+    objectopt.addEventListener('mouseout', function() {
+        mostrar_ocultar()
+    })
+};
+
+function mostrar_ocultar() {
+    if (opciones[objectid-1].style.display == "none") {
+        opciones[objectid-1].style.display = "block";
+    }
+    else {
+        opciones[objectid-1].style.display = "none";
+    }
+}
