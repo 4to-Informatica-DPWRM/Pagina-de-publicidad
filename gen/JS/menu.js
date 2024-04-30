8a0b5e5ea3a68330af06184522328d63970b7e9e
Greem3 <ian.pichardo@hotmail.com>
2024-04-30 09:54:02 -0400

    Orden Archivos
    Chatbot y Menu para todas las paginas y mejor orden de archivos

---
+++ gen/JS/menu.js
@@ -0,0 +1,124 @@
+//#*    [ VARIABLES ]
+
+const documento = document.documentElement;
+const documentostyle = window.getComputedStyle(document.documentElement);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = `<header>
+        <a href="/index.html"><img src="/assets/img/valores.webp" alt="Logo" class="logo"></a>
+        <button id="actmenu">M</button>
+        <div id="menu">
+            <div class="seccion">
+                <a href="/index.html"><p class="menuopts" id="1">Inicio</p></a>
+                <div id="optinicio" class="opciones">
+                    <p>Misión</p>
+                    <p>Visión</p>
+                    <a href="/HTML/valores2.html"><p>Valores</p></a>
+                </div>
+            </div>
+            <div class="seccion">
+                <a href="/HTML/Supermerc.html"><p class="menuopts" id="2">Supermercados</p></a>
+                <div id="optsuper" class="opciones">
+                    <p>Aprezio</p>
+                    <p>Bravo</p>
+                    <p>Sirena</p>
+                    <p>Supermix</p>
+                    <p>Super mercado nacional</p>
+                </div>
+            </div>
+            <div class="seccion">
+                <p class="menuopts" id="3">Farmacias</p>
+                <div id="optfarm" class="opciones">
+                    <a href="https://farmaciasloshidalgos.com.do"><p>Los Hidalgos</p></a>
+                    <p>Farmacia Carolx</p>
+                    <p>Farmacia GBC</p>
+                    <p>Farmanax</p>
+                </div>
+            </div>
+            <p class="menuopts" id="4">Loteria</p>
+            <a href="/HTML/Cines.html"><p class="menuopts" id="5">Cine</p></a>
+            <p class="menuopts" id="6">Mercancia</p>
+            <p class="menuopts" id="7">Nosotros</p>
+        </div>
+    </header>`;
+
+    var Elemento = document.querySelector('header');
+
+    if (Elemento) {
+
+        //? Crea un contenedor temporal, añade el menú y luego lo inserta antes del elemento de referencia
+        let tempDiv = document.createElement('div');
+        tempDiv.innerHTML = menu;
+
+        while (tempDiv.firstChild) {
+            Elemento.parentNode.insertBefore(tempDiv.firstChild, Elemento);
+        }
+    }
+    else {
+        //? Si no hay un elemento existente lo añade al inicio
+        document.body.insertAdjacentHTML('afterbegin', menu);
+    }
+
+    var objectid
+    const allmenu = document.querySelector('#menu');
+    const menuhead = document.querySelectorAll('.menuopts');
+    const iniciooptions = document.getElementById('optinicio');
+    const opciones = document.querySelectorAll('.opciones');
+    const botonformenu = document.querySelector('#actmenu');
+    const openchat = document.querySelector('#chatbot');
+    const chatwithbot = document.getElementsByClassName('chatbot')[0];
+
+    //#*              [ FUNCIONES ]
+
+    //? Mostrar u Ocultar el objeto insertado en el parametro
+    function mostraruocultar(objeto) {
+        
+        if (objeto.style.display == 'none') {
+            objeto.style.display = 'block';
+        }
+        else {
+            objeto.style.display = 'none';
+        };
+    };
+
+    //#*                [ EVENTOS ]
+
+    //? (REPONSIVE. Muestra el menu completo)
+    botonformenu.addEventListener('click', function() {
+
+        mostraruocultar(allmenu);
+    })
+
+    //? Añadir eventos a los 3 primeros objetos del menu, junto con sus opciones
+    for (i = 0; i <= menuhead.length-5; i++) {
+
+        let object = menuhead.item(i);
+        let objectopt = opciones.item(i);
+
+        object.addEventListener('mouseover', function() {
+            objectid = Number(event.target.id)
+
+            opciones[objectid-1].style.display = 'block';
+        });
+
+        object.addEventListener('mouseout', function() {
+
+            opciones[objectid-1].style.display = 'none';
+        });
+
+        //@? Es para evitar que las opciones se cierren cuando muevas el mouse del menu
+        objectopt.addEventListener('mouseover', function() {
+            
+            opciones[objectid-1].style.display = "block";
+        });
+
+        objectopt.addEventListener('mouseout', function() {
+            opciones[objectid-1].style.display = "none";
+        });
+    };
+
+    openchat.addEventListener('click', function() {
+
+        mostraruocultar(chatwithbot)
+    })
+})
